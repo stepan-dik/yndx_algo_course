4a2d291e4b26476d0d29274738daf7beddfb597a
Степан Дик <sdik@open-s.info>
2022-09-02 23:57:18 +0300

    added: solution with priority_queue

--- src/sprint_6/expensive_network/expensive_network.cpp
+++ src/sprint_6/expensive_network/expensive_network.cpp
@@ -11,31 +11,28 @@
  * не обрабатывается, происходит переход к следующей итерации. В
  * противном случае, указанное ребро(A) считается добавленным в
  * остов, его 'вес' добавляется к общему, новая вершина(V1)
- * записывается, как посещённая. Каждое ребро(E), инциндентное ранее
- * непосещенной вершине(V1) из ребра(A), полученного из списка,
- * проверяется на оптимальность по весу для второй вершины(V2),
- * инциндентной этому же ребру(E), вершина(V2), инциндентная новому
- * ребру(E), в свою очередь, должна быть ранее не посещенной. При
- * выполнении этих условий новое ребро (E) добавляется в список(L)
- * и записывается, как оптимальное для новой вершины.
+ * записывается, как посещённая. У каждого ребра(E), инциндентного
+ * ранее непосещенной вершине(V1) из ребра(A) и новой вершине(V2)
+ * новая вершина проверяется была ли она посещена. Если нет, то новое
+ * ребро (E) добавляется в список(L).
  *   Итерации продолжаются, пока список не станет пустым.
  *
  *
  *              --- ВРЕМЕННАЯ СЛОЖНОСТЬ ---
- *   Так как список обрабатываемых рёбер реализован при помощи `set`,
- * являющегося бинарным деревом, операции по вставке и удалению рёбер
- * занимают O(Log(E)), где E - число рёбер. Доступ к элементам
+ *   Так как список обрабатываемых рёбер реализован при помощи
+ * `priority_queue`, являющегося кучей, операции по вставке и удалению
+ * рёбер занимают O(Log(E)), где E - число рёбер. Доступ к элементам
  * массивов, в которых содержится информация о рёбрах и вершинах O(1).
  * Для V вершин и E рёбер, соответственно, временная сложность
  * составит O((E+V)*Log(E)).
  *
  *
  *              --- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ ---
- *   Хранение списков рёбер, оптимальных вершин, списка смежности с
- * индексами рёбер для вершин и занимает O(V+E) памяти, где V вершины
- * и E рёбра. В худшем случае, хранение рёбер для очереди обработки
- * составит O(E), если все рёбра инцидентны одной вершине. Суммарно
- * это составляет пространственную сложность O(V+E).
+ *   Хранение списков рёбер, списка смежности с индексами рёбер для
+ * вершин занимает O(V+E) памяти, где V вершины и E рёбра. В худшем
+ * случае, хранение рёбер для очереди обработки составит O(E), если
+ * все рёбра инцидентны одной вершине. Суммарно это составляет
+ * пространственную сложность O(V+E).
  */
 
 
@@ -44,8 +41,56 @@
 #include <set>
 #include <algorithm>
 #include <tuple>
+#include <queue>
 
 std::pair<int, int> max_tree_weight(
+        int v,
+        const std::vector<int>& start,
+        const std::vector<int>& finish,
+        const std::vector<int>& weight,
+        const std::vector<std::vector<int>>& adj_list)
+{
+    const auto cmp = [weight](const int lhs, const int rhs) {
+        if (weight[lhs] != weight[rhs])
+            return weight[lhs] < weight[rhs];
+        else
+            return lhs > rhs;
+    };
+    std::priority_queue<int, std::vector<int>, decltype(cmp)>
+            edges(cmp);
+    std::vector<bool> used(v+1, false);
+    used[0] = true;
+
+    int wt = 0;
+    int ctr = 0;
+    edges.push(start.size()-1);
+
+    while (!edges.empty())
+    {
+        int new_edge = edges.top();
+        edges.pop();
+        if ((used[start[new_edge]] && used[finish[new_edge]]) ||
+                start[new_edge] == finish[new_edge])
+            continue;
+
+        int cur_v = used[start[new_edge]] ?
+                    finish[new_edge] : start[new_edge];
+        wt += weight[new_edge];
+        ++ctr;
+        used[cur_v] = true;
+
+        for (const auto& edge_idx: adj_list[cur_v]) {
+            int fin = cur_v == start[edge_idx] ?
+                        finish[edge_idx] : start[edge_idx];
+            if (!used[fin])
+                edges.push(edge_idx);
+        }
+    }
+
+    return {wt, ctr};
+}
+
+std::pair<int, int> max_tree_weight_set(
         int v,
         const std::vector<int>& start,
         const std::vector<int>& finish,
@@ -61,7 +106,6 @@ std::pair<int, int> max_tree_weight(
     std::set<int, decltype(cmp)> edges(cmp);
     std::vector<bool> used(v+1, false);
     used[0] = true;
-    std::vector<int> best_edges(used.size(), -1);
 
     int wt = 0;
     int ctr = 0;
@@ -84,16 +128,8 @@ std::pair<int, int> max_tree_weight(
         for (const auto& edge_idx: adj_list[cur_v]) {
             int fin = cur_v == start[edge_idx] ?
                         finish[edge_idx] : start[edge_idx];
-            if (!used[fin] &&
-                    (best_edges[fin] == -1 ||
-                     weight[edge_idx] > weight[best_edges[fin]]))
-            {
-                if (best_edges[fin] != -1)
-                    edges.erase(best_edges[fin]);
-
+            if (!used[fin])
                 edges.insert(edge_idx);
-                best_edges[fin] = edge_idx;
-            }
         }
     }
 
